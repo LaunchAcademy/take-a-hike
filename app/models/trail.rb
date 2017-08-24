149e55737d6ad93fc7c1c2cf6013f9893bb30e3e
Dan Pickett <dan.pickett@launchacademy.com>
2017-08-23 21:02:03 -0400

    fix bad/extraneous comma

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,6 +1,6 @@
 class Trail < ActiveRecord::Base
   validates :name, presence: true
-  validates :length, numericality: true,
+  validates :length, numericality: true
   validates :city, presence: true
   validates :state, presence: true
   validates :postal_code, presence: true
