423195ee550c8efdc349e2a416e65ee1f3be19af
Dan Pickett <dan.pickett@launchacademy.com>
2017-08-24 18:36:23 -0400

    update configuration to adhere to new shoulda DSL

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,13 @@ require_relative 'support/database_cleaner'
 require 'valid_attribute'
 require 'shoulda/matchers'
 
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :active_record
+  end
+end
+
 configure :test do
   set :environment, :test
   set :database, :test
@@ -26,4 +33,7 @@ RSpec.configure do |config|
 
   config.order = :random
   Kernel.srand config.seed
+
+  config.include(Shoulda::Matchers::ActiveModel, type: :model)
+  config.include(Shoulda::Matchers::ActiveRecord, type: :model)
 end
